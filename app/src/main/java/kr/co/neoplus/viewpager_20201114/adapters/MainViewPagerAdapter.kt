c3e9ed730daa29a436ff8158878865da5be985fe
kimhaun <wha02068@gmail.com>
2020-11-14 15:43:33 +0900

    뷰페이저에 프래그먼트들을 위체이 맞게 뿌려주는 어댑터 작성

---
+++ app/src/main/java/kr/co/neoplus/viewpager_20201114/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package kr.co.neoplus.viewpager_20201114.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kr.co.neoplus.viewpager_20201114.fragments.AddressFragment
+import kr.co.neoplus.viewpager_20201114.fragments.BirthYearFragment
+import kr.co.neoplus.viewpager_20201114.fragments.NameFragment
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    //뷰페이저가 총 몇 장 짜리인지를 정수로 리턴
+    override fun getCount(): Int {
+        return 3
+    }
+
+    //위치에 맞는 프래그먼트 리턴
+    override fun getItem(position: Int): Fragment {
+         if(position == 0){
+             return NameFragment()
+         }
+        else if(position == 1){
+             return BirthYearFragment()
+         }
+        else{
+             return AddressFragment()
+         }
+    }
+}
