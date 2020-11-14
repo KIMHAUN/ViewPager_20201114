0249974c1bfdd9ce6713f91c3e4f58e78130f81e
kimhaun <wha02068@gmail.com>
2020-11-14 15:19:39 +0900

    이름/생년/주소 표시하는 프래그먼트들 추가

---
+++ app/src/main/java/kr/co/neoplus/viewpager_20201114/fragments/BirthYearFragment.kt
@@ -0,0 +1,19 @@
+package kr.co.neoplus.viewpager_20201114.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.neoplus.viewpager_20201114.R
+
+class BirthYearFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_birth_year, container, false )
+    }
+
+}
